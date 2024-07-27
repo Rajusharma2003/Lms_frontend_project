03a4e8029510b2999ce8f4bd553083b5e53a88a6
Rajusharma2003 <rajusharma50154@gmail.com>
2024-07-27 12:08:59 +0530

    complete the denied page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import AboutUs from './Pages/AboutUs';
 import ContactPage from './Pages/ContactUs';
 import CourseList from './Pages/Courses/CoursePage';
+import Denied from './Pages/Denied';
 import HomePage from './Pages/HomePage';
 import Login from './Pages/Login';
 import NotFound from './Pages/NotFound';
@@ -18,6 +19,7 @@ function App() {
         <Route path="/about" element={<AboutUs />} ></Route>
         <Route path="/contact" element={<ContactPage />} ></Route>
         <Route path="/courses" element={<CourseList/>} ></Route>
+        <Route path="/denied" element={<Denied/>} ></Route>
 
         <Route path="/signup" element={<Signup/>} />
         <Route path="/login" element={<Login/>} />
