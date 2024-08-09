f60d606d67cd9e2b2e246c8d6efc0fb00589e452
Rajusharma2003 <rajusharma50154@gmail.com>
2024-08-09 17:08:44 +0530

    Helper folder change url

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:3008/api/v1";
-const BASE_URL = "https://lmsbackend-m6fr.onrender.com";
+const BASE_URL = "https://lmsbackend-m6fr.onrender.com/api/v1";
 
 const axiosInstance = axios.create();
 
